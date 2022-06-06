355ac3f7e74ebef3f9cbf4fb848364e3791f0cd9
Ivan Hofer <ivan.hofer@outlook.com>
2022-06-06 10:21:34 +0200

    setup eslint + prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,21 @@ module.exports = {
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'svelte3/typescript': () => require('typescript'),
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
-	}
-};
+		node: true,
+	},
+	rules: {
+		indent: 'off',
+		'no-console': 'error',
+		'prettier/prettier': ['warn'],
+		'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+	},
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#  :first_quarter_moon:	sthemer
+# :first_quarter_moon: sthemer
 
 **An opinionated solution to support multiple themes in your Svelte/SvelteKit application.**
 
