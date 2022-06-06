fdff5718abd3e0783dde3822f99bfc7a42e07f60
Ivan Hofer <ivan.hofer@outlook.com>
2022-06-06 22:41:38 +0200

    feat: add scss mixins

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,11 @@ import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		scss: {
+			prependData: `@import 'src/lib/sthemer.scss';`,
+		},
+	}),
 
 	kit: {
 		adapter: adapter(),
