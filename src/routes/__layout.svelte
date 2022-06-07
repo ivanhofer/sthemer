196a691ffa2f6d09aecfd67dfbd4a144307f87d1
Ivan Hofer <ivan.hofer@outlook.com>
2022-06-07 16:46:05 +0200

    chore: add simple wrapper for website

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Sthemer from '$lib/Sthemer.svelte'
+</script>
+
+<Sthemer>
+	<main>
+		<slot />
+	</main>
+</Sthemer>
+
+<style lang="scss">
+	:global {
+		@import '../styles/reset';
+	}
+
+	main {
+		min-height: 100vh;
+
+		@include on-dark {
+			background: #000;
+		}
+
+		@include on-light {
+			background: #fff;
+		}
+	}
+</style>
