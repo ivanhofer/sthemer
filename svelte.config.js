721e35ab1a7f4779c600e3cd1ad5999909085ac3
Ivan Hofer <ivan.hofer@outlook.com>
2022-06-07 16:06:05 +0200

    feat: add support for `less` and `sass`

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,13 @@ import preprocess from 'svelte-preprocess'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({
+		less: {
+			prependData: `@import 'src/lib/mixins';`,
+		},
 		sass: {
 			prependData: `@import 'src/lib/mixins'`,
 		},
 		scss: {
-			prependData: `@import 'src/lib/mixins';`,
-		},
-		less: {
 			prependData: `@import 'src/lib/mixins';`,
 		},
 	}),
