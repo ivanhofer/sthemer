5d717db768fe967dd3c027616c83762e103d484a
Ivan Hofer <ivan.hofer@outlook.com>
2022-06-21 11:07:50 +0200

    feat: create context cjs file

---
+++ esbuild.ts
@@ -0,0 +1,18 @@
+import { build } from 'esbuild/lib/main'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const getPath = (file: string) => resolve(__dirname, file)
+
+build({
+	entryPoints: ['./package/context.js'],
+	bundle: true,
+	outfile: getPath(`./package/context.cjs`),
+	platform: 'browser',
+	format: 'cjs',
+	tsconfig: './tsconfig.json',
+	allowOverwrite: true,
+}).catch(() => process.exit(1))
